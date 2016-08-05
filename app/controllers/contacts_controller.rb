be9e1899a7cbb52904d32a50edf1b9ccb3588c10
Angela Ji <angelasji@gmail.com>
2016-08-05 01:30:27 -0700

    issues with mailer resolved and edited mailer html/text messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,7 @@ class ContactsController < ApplicationController
     email = params[:email]
     message = params[:message]
     @profile = Profile.find params[:profile_id]
-    ContactsMailer.notify_student(@profile.user, name, message).deliver_now
+    ContactsMailer.notify_student(@profile.user, name, email, message).deliver_now
     redirect_to root_url, notice: "Email has been sent to the student."
   end
 
